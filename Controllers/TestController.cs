34dc77832c02c36568b80ce656c0c64dd59e6fd0
Martin <martinpetoffer@pm.me>
2024-10-02 22:46:40 +0300

    Polymorphic bug

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -7,22 +7,10 @@ namespace OpenApiStuff.Controllers;
 [Route("[controller]")]
 public class TestController : ControllerBase
 {
-    [HttpGet]
-    public IEnumerable<Entity> Get()
+    [HttpGet("component")]
+    public ComponentDto GetComponent()
     {
-        return
-        [
-            new Company
-            {
-                Name = "Microsoft"
-            },
-            new Person
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                Gender = Gender.Male
-            }
-        ];
+        return new SectionDto();
     }
     
     [HttpGet("section")]
@@ -30,65 +18,11 @@ public class TestController : ControllerBase
     {
         return new SectionDto();
     }
-    
-    [HttpGet("query")]
-    public IEnumerable<Entity> Search(Gender gender = Gender.Female)
-    {
-        return [];
-    }
 }
 
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
-[JsonDerivedType(typeof(Person), typeDiscriminator: "Person")]
-[JsonDerivedType(typeof(Company), typeDiscriminator: "Company")]
-public abstract class Entity
-{
-    public List<string> Values => [];
-}
-
-public class Person : Entity
-{
-    public required string FirstName { get; init; }
-    
-    public required string LastName { get; init; }
-    
-    public required Gender Gender { get; init; }
-}
-
-public class Company : Entity
-{
-    public required string Name { get; init; }
-}
-
-public enum Gender
-{
-    Male,
-    Female
-}
-
-
-[JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
-[JsonDerivedType(typeof(TextDto), typeDiscriminator: "text")]
-[JsonDerivedType(typeof(TableDto), typeDiscriminator: "table")]
 [JsonDerivedType(typeof(SectionDto), typeDiscriminator: "section")]
-public abstract class ContentDto;
-
-[JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
-[JsonDerivedType(typeof(TableDto), typeDiscriminator: "table")]
-[JsonDerivedType(typeof(SectionDto), typeDiscriminator: "section")]
-public abstract class ComponentDto : ContentDto;
-
-public class TextDto : ContentDto
-{
-    public required string Value { get; init; }
-}
-
-public class TableDto : ComponentDto
-{
-    public required string Title { get; init; }
-
-    public IEnumerable<ContentDto> Contents { get; init; } = [];
-}
+public abstract class ComponentDto;
 
 public class SectionDto : ComponentDto
 {
